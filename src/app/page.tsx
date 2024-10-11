914c93e74055885db9c3c3b374ed318d89d8145a
NeelContractor <neelcontractor7@gmail.com>
2024-10-11 19:25:20 +0530

    first commit still few error are felt to solve

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,208 @@
-import Image from "next/image";
+"use client"
+import { useWallet } from "@solana/wallet-adapter-react";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { Connection, VersionedTransaction } from "@solana/web3.js";
+import React, { useCallback, useEffect, useState } from "react";
+
+const assets = [
+  {name: 'SOL', mint: 'So11111111111111111111111111111111111111112', decimals: 9},
+  { name: 'USDC', mint: 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v', decimals: 6},
+  { name: 'BONK', mint: 'DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263', decimals: 5 },
+  { name: 'WIF', mint: 'EKpQGSJtjMFqKZ9KQanSqYXRcF8fBopzLHYxdM65zcjm', decimals: 6},
+];
+
+const debaounce = <T extends unknown[]>(
+  func: (...args: T) => void,
+  wait: number
+) => {
+  let timeout: NodeJS.Timeout | undefined;
+
+  return (...args: T) => {
+    const later = () => {
+      clearTimeout(timeout);
+      func(...args);
+    };
+
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  }
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [fromAsset, setFromAsset] = useState(assets[0]);
+  const [toAsset, setToAsset] = useState(assets[1]);
+  const [fromAmount, setFromAmount] = useState(0);
+  const [toAmount, setToAmount] = useState(0);
+  const [quoteResponse, setQuoteResponse] = useState(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const wallet = useWallet();
+
+  const connection = new Connection('https://api.devnet.solana.com');
+
+  const handleFromAssetChange = async (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setFromAsset(
+      assets.find((asset) => asset.name === event?.target.value) || assets[0]
+    )
+  }
+
+  const handleToAssetChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setToAsset(
+      assets.find((asset) => asset.name === event.target.value) || assets[0]
+    )
+  }
+
+  const handleFromValueChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setFromAmount(Number(event.target.value));
+  }
+
+  const debaounceQuoteCall = useCallback(debaounce(getQuote, 500), []);
+
+  useEffect(() => {
+    debaounceQuoteCall(fromAmount);
+  }, [fromAmount, debaounceQuoteCall]);
+
+  async function getQuote(currentAmount: number) {
+    if (isNaN(currentAmount) || currentAmount <= 0) {
+      console.error(`Invalid fromAmount value: ${currentAmount}`);
+      return;
+    
+    }
+    const quote = await (
+      await fetch(
+        `https://quote-api.jup.ag/v6/quote?inputMint=${fromAsset.mint}&outputMint=${toAsset.mint}&amount=${currentAmount * Math.pow(10, fromAsset.decimals)}&slippage=0.5`
+      )
+    ).json();
+
+    if(quote && quote.outAmount) {
+      const outAmountNumber = 
+        Number(quote.outAmount) / Math.pow(10, toAsset.decimals);
+      setToAmount(outAmountNumber);
+    }
+    setQuoteResponse(quote)
+  }
+
+  async function signAndSendTransaction() {
+    if (!wallet.connected || !wallet.signTransaction) {
+      console.error(
+        "Wallet is not connected or does not support signing transactions"
+      );
+      return;
+    }
+    const { swapTransaction } = await (
+      await fetch('https://quote-api.jup.ag/v6/swap', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          quoteResponse,
+          userPublicKey: wallet.publicKey?.toString(),
+          wrapAndUnwarpSol: true,
+        })
+      })
+    ).json();
+
+    try {
+      const swapTransactionBuf = Buffer.from(swapTransaction, 'base64');
+      const transaction = VersionedTransaction.deserialize(swapTransactionBuf);
+      const signedTransaction = await wallet.signTransaction(transaction);
+
+      const rawTransaction = signedTransaction.serialize();
+      const txid = await connection.sendRawTransaction(rawTransaction, {
+        skipPreflight: true,
+        maxRetries: 2,
+      });
+
+      const latestBlockHash = await connection.getLatestBlockhash();
+      await connection.confirmTransaction({
+        blockhash: latestBlockHash.blockhash,
+        lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+        signature: txid
+      }, 'confirmed')
+
+      console.log(`https://solscan.io/tx/${txid}`);
+    } catch (e) {
+      console.error(`Error signing or sending the transaction: ${e}`);
+    }
+  }
+
+
+  // useEffect(() => {
+  //   if (connected) {
+  //     console.log("Wallet Connected!");
+  //   } else {
+  //     console.log("Please Connect Your wallet")
+  //   }
+  // }, [connected, wallet]);
+
+  return (
+    <div className="grid">
+      <div className="flex justify-between p-4">
+      <div className="font-bold text-2xl self-center">Swap Token</div>
+      <div>
+        <WalletMultiButton  />
+      </div>
     </div>
+    <main className="flex items-center justify-center min-h-screen">
+      <div className="border border-gray-600 p-20 rounded-lg">
+        <div>
+          <div>
+            <h3 className="font-semibold text-lg">You Pay</h3>
+            <input 
+              type="number" 
+              value={fromAmount}
+              onChange={handleFromValueChange}
+              className="bg-gray-800 rounded-lg outline-none p-2 w-96" 
+            />
+            <select 
+              value={fromAsset.name}
+              onChange={handleFromAssetChange}
+              className=""
+            >
+              {assets.map((asset) => (
+                <option key={asset.mint} value={asset.name}>
+                  {asset.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <h3>You Receive</h3>
+            <input 
+              type="number" 
+              value={toAmount}
+              readOnly
+              className="bg-gray-800 rounded-lg outline-none p-2 w-96" 
+            />
+            <select 
+              value={fromAsset.name}
+              onChange={handleToAssetChange}
+              className=""
+            >
+              {assets.map((asset) => (
+                <option key={asset.mint} value={asset.name}>
+                  {asset.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <button 
+              onClick={signAndSendTransaction}
+              className=""
+              disabled={toAsset.mint === fromAsset.mint}  
+            >
+              Swap
+            </button>
+          </div>
+        </div>
+      </div>
+    </main>
+    </div>
+    
   );
 }
---
+++ src/components/AppWalletProvider.tsx
@@ -0,0 +1,27 @@
+"use client";
+ 
+import React from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+
+ 
+require("@solana/wallet-adapter-react-ui/styles.css");
+
+ 
+export default function AppWalletProvider({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+   
+    return (
+      <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+        <WalletProvider wallets={[]} autoConnect>
+          <WalletModalProvider>{children}</WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    );
+  }
