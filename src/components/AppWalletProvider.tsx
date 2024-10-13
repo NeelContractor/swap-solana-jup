45ad26f7b51f75b5c87e9490bfb51d4b2922cc4c
NeelContractor <neelcontractor7@gmail.com>
2024-10-13 18:01:50 +0530

    solved lint errors

--- src/components/AppWalletProvider.tsx
+++ src/components/AppWalletProvider.tsx
@@ -8,7 +8,7 @@ import {
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 
  
-require("@solana/wallet-adapter-react-ui/styles.css");
+import "@solana/wallet-adapter-react-ui/styles.css";
 
  
 export default function AppWalletProvider({
